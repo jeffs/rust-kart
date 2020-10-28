27ff1bd27ebcf75b1993befdaa160df43d9d53f5
Jeff Schwab <jeff@unbuggy.com>
2020-10-28 13:19:52 -0400

    Factor arg parsing into small functions

--- src/bin/len.rs
+++ src/bin/len.rs
@@ -1,7 +1,7 @@
 #[macro_use]
 extern crate clap;
 
-use clap::{App, Arg};
+use clap::App;
 
 #[derive(Debug)]
 struct Args {
@@ -11,35 +11,30 @@ struct Args {
     files: Vec<String>,
 }
 
-fn flag<'help>(name: &'help str, about: &'help str) -> Arg<'help> {
-    debug_assert_eq!(name.len(), 1);
-    Arg::new(name)
-        .about(about)
-        .short(name.chars().next().unwrap())
-}
-
-fn parse_args() -> Args {
-    let matches = App::new("Len")
+fn new_app() -> App<'static> {
+    App::new("Len")
         .version(crate_version!())
         .help_about("Print help information")
         .version_about("Print version information")
-        .arg(flag("1", "Print only the first line of output"))
-        .arg(flag("r", "Sort lines by decreasing length"))
-        .arg(flag("s", "Sort lines by length"))
-        .arg(
-            Arg::new("file")
-                .about("Files to parse instead of stdin")
-                .multiple(true),
-        )
-        .get_matches();
+        .arg("-1 'Print only the first line of output'")
+        .arg("-r 'Sort lines by decreasing length'")
+        .arg("-s 'Sort lines by length'")
+        .arg("[file]... 'Files to parse instead of stdin'")
+}
 
+fn to_vec(values: Option<clap::Values>) -> Vec<String> {
+    values.map_or(vec![], |values| {
+        values.map(|s| s.to_string()).collect::<Vec<_>>()
+    })
+}
+
+fn parse_args() -> Args {
+    let matches = new_app().get_matches();
     Args {
         one: matches.is_present("1"),
         r: matches.is_present("r"),
         s: matches.is_present("s"),
-        files: matches.values_of("file").map_or(vec![], |values| {
-            values.map(|s| s.to_string()).collect::<Vec<_>>()
-        }),
+        files: to_vec(matches.values_of("file")),
     }
 }
 
