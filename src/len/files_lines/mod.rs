5a20929cf80901d8294df3437df39e074c691320
Jeff Schwab <jeff@unbuggy.com>
2020-11-07 03:38:17 -0500

    Reimplement FilesLines as a state machine
    I am not convinced I prefer this.

--- src/len/files_lines/mod.rs
+++ src/len/files_lines/mod.rs
@@ -1,8 +1,11 @@
+#![allow(dead_code, unused_imports)]
+
 mod tests;
 
 use std::collections::VecDeque;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Lines};
+use std::mem;
 use std::path::{Path, PathBuf};
 
 fn prefix_error<P>(path: P, err: io::Error) -> io::Error
@@ -13,14 +16,106 @@ where
     io::Error::new(err.kind(), what)
 }
 
+struct Opening {
+    path: PathBuf,
+    lines: Lines<BufReader<File>>,
+}
+
+fn open_file<P>(path: P) -> io::Result<Opening>
+where
+    P: AsRef<Path>,
+{
+    let path = path.as_ref().to_owned();
+    let file = File::open(&path)?;
+    let lines = io::BufReader::new(file).lines();
+    Ok(Opening { path, lines })
+}
+
+struct InitState {
+    paths: VecDeque<PathBuf>,
+}
+
+struct OpenState {
+    paths: VecDeque<PathBuf>,
+    open: Opening,
+}
+
+enum State {
+    Init(InitState),
+    Open(OpenState),
+    Done,
+}
+
+impl State {
+    fn init(paths: VecDeque<PathBuf>) -> State {
+        State::Init(InitState { paths })
+    }
+
+    fn open(paths: VecDeque<PathBuf>, open: Opening) -> State {
+        State::Open(OpenState { paths, open })
+    }
+
+    fn from_init(mut value: InitState) -> (State, Option<io::Error>) {
+        if let Some(path) = value.paths.pop_front() {
+            match open_file(&path) {
+                Ok(open) => (State::open(value.paths, open), None),
+                Err(err) => {
+                    let err = prefix_error(&path, err);
+                    (State::init(value.paths), Some(err))
+                }
+            }
+        } else {
+            (State::Done, None)
+        }
+    }
+
+    fn from_open(mut value: OpenState) -> (State, Option<io::Result<String>>) {
+        match value.open.lines.next() {
+            next@Some(Ok(_)) => (State::Open(value), next),
+
+            Some(Err(err)) => {
+                let err = prefix_error(&value.open.path, err);
+                (State::init(value.paths), Some(Err(err)))
+            }
+
+            None => {
+                let (state, err) = State::from_init(InitState { paths: value.paths });
+                match err {
+                    Some(err) => (state, Some(Err(err))),
+                    None => State::next(state),
+                }
+            }
+        }
+    }
+
+    fn next(state: State) -> (State, Option<io::Result<String>>) {
+        match state {
+            State::Init(value) => {
+                let (state, err) = State::from_init(value);
+                match err {
+                    Some(err) => (state, Some(Err(err))),
+                    None => State::next(state),
+                }
+            }
+            State::Open(value) => State::from_open(value),
+            State::Done => (State::Done, None),
+        }
+    }
+}
+
+impl Default for State {
+    fn default() -> Self {
+        State::Done
+    }
+}
+
 /// Iterates over the lines of a sequence of files.
 ///
 /// Reports all errors, but:
 /// * After any encoding error, skips the remainder of the current file.
 /// * After any other error, returns None.
 pub struct FilesLines {
-    paths: VecDeque<PathBuf>,
-    lines: Option<Lines<BufReader<File>>>,
+    state: State,
 }
 
 impl FilesLines {
@@ -29,32 +124,10 @@ impl FilesLines {
         P: AsRef<Path>,
         I: IntoIterator<Item = P>,
     {
-        FilesLines {
-            paths: paths.into_iter().map(|p| p.as_ref().to_owned()).collect(),
-            lines: None,
-        }
-    }
-
-    /// Opens the next file if no file is already open, and if another path is
-    /// available, assigning a line iterator to self.lines.  On failure to open
-    /// a file, returns the error.
-    fn open_lines(&mut self) -> io::Result<()> {
-        if self.lines.is_none() && !self.paths.is_empty() {
-            match File::open(&self.paths[0]) {
-                Ok(file) => {
-                    self.lines = Some(io::BufReader::new(file).lines());
-                }
-                Err(err) => {
-                    return Err(err);
-                }
-            }
-        }
-        Ok(())
-    }
-
-    fn skip_path(&mut self) -> PathBuf {
-        self.lines = None;
-        self.paths.pop_front().unwrap()
+        let paths = paths.into_iter();
+        let paths = paths.map(|p| p.as_ref().to_owned());
+        let state = State::init(paths.collect());
+        FilesLines { state }
     }
 }
 
@@ -62,26 +135,8 @@ impl Iterator for FilesLines {
     type Item = io::Result<String>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        loop {
-            if let Err(err) = self.open_lines() {
-                return Some(Err(prefix_error(self.skip_path(), err)));
-            }
-
-            if self.lines.is_none() {
-                return None;
-            }
-
-            let opt = self.lines.as_mut().and_then(|lines| lines.next());
-            if let Some(Ok(_)) = opt {
-                // We got a line.  Return it.
-                return opt;
-            } else if let Some(Err(err)) = opt {
-                // We got an error.  Return it and skip the rest of the file.
-                return Some(Err(prefix_error(self.skip_path(), err)));
-            } else {
-                // We got nothing.  Proceed to the next file, if any.
-                self.skip_path();
-            }
-        }
+        let (state, next) = State::next(mem::take(&mut self.state));
+        self.state = state;
+        next
     }
 }
