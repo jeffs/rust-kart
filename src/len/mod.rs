44d8d088b474c539ec03a077b7f2c42425255bfc
Jeff Schwab <jeff@unbuggy.com>
2020-11-09 01:50:12 -0500

    Return non-zero exit status on hard error

--- src/len/mod.rs
+++ src/len/mod.rs
@@ -15,37 +15,50 @@ use std::io::{self, BufRead};
 use std::path::PathBuf;
 use take_until::TakeUntilExt;
 
-fn is_hard_err(res: &io::Result<String>) -> bool {
-    res.as_ref()
-        .err()
-        .filter(|err| err.kind() != io::ErrorKind::InvalidData)
-        .is_some()
+enum Status {
+    Success,
+    Failure,
 }
 
-fn print<I>(lines: I, log: &Log)
+fn is_hard_err(res: &io::Result<String>) -> bool {
+    match res {
+        Err(err) if err.kind() != io::ErrorKind::InvalidData => true,
+        _ => false,
+    }
+}
+
+fn vent<I>(lines: I, log: Log) -> Status
 where
     I: IntoIterator<Item = io::Result<String>>,
 {
-    for res in lines {
+    for res in lines.into_iter() {
         match res {
-            Ok(line) => println!("{}:{}", line.chars().count(), line),
-            Err(err) if is_hard_err(&res) => log.error(err),
-            Err(err) => log.warning(err),
+            Ok(line) => {
+                println!("{}:{}", line.chars().count(), line);
+            }
+            Err(err) if is_hard_err(&res) => {
+                log.error(err);
+                return Status::Failure;
+            }
+            Err(err) => {
+                log.warning(err);
+            }
         }
     }
+    Status::Success
 }
 
 /// Performs the specified operation on the specified input lines, and prints
 /// the results to stdout.  Any Error from the specified lines is printed to
 /// the specified log.
-fn execute<I>(op: Op, lines: I, log: Log)
+fn execute<I>(op: Op, lines: I, log: Log) -> Status
 where
     I: IntoIterator<Item = io::Result<String>>,
 {
     match op {
-        Op::All => print(lines, &log),
-        Op::Max => print(longest(lines).take_until(is_hard_err), &log),
-        Op::Min => print(shortest(lines).take_until(is_hard_err), &log),
+        Op::All => vent(lines, log),
+        Op::Max => vent(longest(lines), log),
+        Op::Min => vent(shortest(lines), log),
         // Op::One,
         // Op::ReverseSort,
         // Op::Sort,
@@ -54,26 +67,29 @@ where
 }
 
 /// Stops on the first error.
-fn execute_stdin(op: Op, log: Log) {
+fn execute_stdin(op: Op, log: Log) -> Status {
     let stdin = io::stdin();
     let lines = stdin.lock().lines().take_until(|res| res.is_err());
-    execute(op, lines, log);
+    execute(op, lines, log)
 }
 
 /// Stops on the first error other than InvalidData.  Binary files cause
 /// warnings, but don't cause the program to exit, since we may still find
 /// useful data in subsequent files.
-fn execute_files(op: Op, files: Vec<PathBuf>, log: Log) {
+fn execute_files(op: Op, files: Vec<PathBuf>, log: Log) -> Status {
     let lines = FilesLines::new(files.iter()).take_until(is_hard_err);
-    execute(op, lines, log);
+    execute(op, lines, log)
 }
 
 pub fn main() {
     let command = Command::from_env();
     let log = Log::new(command.color);
-    if command.files.is_empty() {
-        execute_stdin(command.op, log);
+    let status = if command.files.is_empty() {
+        execute_stdin(command.op, log)
     } else {
-        execute_files(command.op, command.files, log);
+        execute_files(command.op, command.files, log)
+    };
+    if let Status::Failure = status {
+        std::process::exit(1);
     }
 }
