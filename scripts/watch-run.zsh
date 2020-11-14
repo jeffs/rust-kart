8ed36902d32076f13858986247ef4bc138f82cb9
Jeff Schwab <jeff@unbuggy.com>
2020-11-14 01:36:04 -0500

    Update watch-run script to work on Mac and Linux

--- scripts/watch-run.zsh
+++ scripts/watch-run.zsh
@@ -5,19 +5,20 @@ trap "rm -rf $tmp" EXIT
 
 declare a=$tmp/a b=$tmp/b
 
-# Print timestamps of regular files under the current directory.
-ts() {
-    fd --type=file --exec-batch stat -f %m
-}
+if [ $(uname) = Linux ]; then
+    # Print timestamps of regular files under the current directory.
+    ts() { fd --type=file --exec-batch stat --format=%Y; }
+else
+    ts() { fd --type=file --exec-batch stat -f %m; }
+fi
 
 clear-run() {
     local run="cargo --color=always --quiet run --bin len"
     clear
-    cargo --color=always build \
-        && clear \
-        && echo -e "\e[2m[$(date +%T)] $run" "$@" "\e[22m" \
-        && echo \
-        && ${=run} "$@" tests/data/utf8/* tests/data/bad tests/data/utf8/*
+    cargo --color=always build || return 1
+    echo -e "\e[2m[$(date +%T)] $run" "$@" "\e[22m\n"
+    ${=run} "$@" tests/data/utf8/* tests/data/bad tests/data/utf8/*
+    echo -ne "\n\e[2m[$(date +%T)] $?\e[22m"
 }
 
 ts >$a
