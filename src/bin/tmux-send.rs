6f60c2b61182d040b924aea3e5bbcb0dc507b24e
Jeff Schwab <jeff@unbuggy.com>
2020-11-16 15:20:24 -0500

    Add program to send its stdin to a tmux pane
    The tmux-send program is a simplistic wrapper around 'tmux send-keys'.
    Piping text to tmux-send is often a bit easier than passing it directly
    to tmux send-keys as a single argument; e.g., from Vim bindings:
    
        " Tmux integration: Send a line, paragraph, or selection to the bottom pane.
        nnoremap <silent> <C-j>     :.w      !tmux-send<CR><CR>j
        nnoremap <silent> <Leader>e vap:w    !tmux-send<CR><CR>}
        vnoremap <silent> <Leader>e :w       !tmux-send<CR><CR>

---
+++ src/bin/tmux-send.rs
@@ -0,0 +1,26 @@
+//! Sends standard input to the bottom-right tmux pane.
+use std::io::{self, Read};
+use std::os::unix::process::ExitStatusExt;
+use std::process::{self, Command, ExitStatus};
+
+fn read_stdin() -> io::Result<Vec<u8>> {
+    let mut bytes = Vec::new();
+    io::stdin().read_to_end(&mut bytes).map(|_| bytes)
+}
+
+fn run() -> Result<ExitStatus, Box<dyn std::error::Error>> {
+    let text = String::from_utf8(read_stdin()?)?;
+    let args = ["send-keys", "-lt", "bottom-right", &text];
+    Ok(Command::new("tmux").args(&args).status()?)
+}
+
+fn main() {
+    let status = run().unwrap();
+
+    #[cfg(unix)]
+    for signal in status.signal() {
+        process::exit(128 + signal);
+    }
+
+    process::exit(status.code().unwrap());
+}
