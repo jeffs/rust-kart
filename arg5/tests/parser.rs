cf037360983b5f7955d25db5a0107a6c90327284
Jeff Schwab <jeff@unbuggy.com>
2021-09-12 17:20:49 -0400

    Futz with test factoring

--- arg5/tests/parser.rs
+++ arg5/tests/parser.rs
@@ -1,33 +1,37 @@
-mod when_no_positional_parameter_is_declared {
-    use arg5::Parser;
+mod a_parser {
+    use arg5::{Parameter, Parser};
 
     #[test]
     fn rejects_an_unexpected_positional_argument() {
         let mut parser = Parser::new();
         assert!(parser.parse([String::from("arg1")]).is_err());
     }
-}
 
-mod when_a_positional_parameter_is_declared {
-    use arg5::{Parameter, Parser};
+    mod given_a_positional_string_parameter {
+        use super::*;
 
-    #[test]
-    fn can_assign_a_positional_string() {
-        let want = "hello";
-        let mut got = String::new();
-        let mut parser = Parser::new();
-        parser.declare_positional(Parameter::new("arg1", &mut got));
-        parser.parse([String::from(want)]).unwrap();
-        assert_eq!(got, want);
+        #[test]
+        fn can_assign_an_argument() {
+            let want = "hello";
+            let mut got = String::new();
+            let mut parser = Parser::new();
+            parser.declare_positional(Parameter::new("arg1", &mut got));
+            parser.parse([String::from(want)]).unwrap();
+            assert_eq!(got, want);
+        }
     }
 
-    #[test]
-    fn can_assign_a_positional_i32() {
-        let want = 42;
-        let mut got = 0;
-        let mut parser = Parser::new();
-        parser.declare_positional(Parameter::new("arg1", &mut got));
-        parser.parse([String::from("42")]).unwrap();
-        assert_eq!(got, want);
+    mod given_a_positional_i32_parameter {
+        use super::*;
+
+        #[test]
+        fn can_parse_and_assign_an_argument() {
+            let want = 42;
+            let mut got = 0;
+            let mut parser = Parser::new();
+            parser.declare_positional(Parameter::new("arg1", &mut got));
+            parser.parse([String::from("42")]).unwrap();
+            assert_eq!(got, want);
+        }
     }
 }
