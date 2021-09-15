b77d60ab2d5b6b1fe05ce00cf9521c1390cd5943
Jeff Schwab <jeff@unbuggy.com>
2021-09-14 21:50:21 -0400

    Test parsing of two positional parameters

--- arg5/tests/parser.rs
+++ arg5/tests/parser.rs
@@ -1,5 +1,5 @@
 mod a_parser {
-    use arg5::Parser;
+    use arg5::{ParseError, Parser};
 
     // The first arg to a program is typically the program name.
     const ARG0: &str = "arg0";
@@ -75,6 +75,42 @@ mod a_parser {
             let mut parser = Parser::new();
             parser.declare_positional("par1", &mut got);
             assert!(parser.parse([ARG0]).is_err());
+        }
+    }
+
+    mod given_two_positional_parameters {
+        use super::*;
+
+        fn try_parse(args: &[&'static str]) -> Result<(i32, String), ParseError> {
+            let mut got_i32 = 0;
+            let mut got_str = String::new();
+            let mut parser = Parser::new();
+            parser.declare_positional("par1", &mut got_i32);
+            parser.declare_positional("par2", &mut got_str);
+            parser.parse(args)?;
+            Ok((got_i32, got_str))
+        }
+
+        #[test]
+        fn can_parse_and_assign_them() {
+            let (got_i32, got_str) = try_parse(&[ARG0, "42", "hello"]).unwrap();
+            assert_eq!(got_i32, 42);
+            assert_eq!(got_str, "hello");
+        }
+
+        #[test]
+        fn rejects_a_third_argument() {
+            assert!(try_parse(&[ARG0, "42", "43", "44"]).is_err());
+        }
+
+        #[test]
+        fn rejects_an_empty_argument_list() {
+            assert!(try_parse(&[ARG0]).is_err());
+        }
+
+        #[test]
+        fn rejects_a_unary_argument_list() {
+            assert!(try_parse(&[ARG0, "42"]).is_err());
         }
     }
 }
