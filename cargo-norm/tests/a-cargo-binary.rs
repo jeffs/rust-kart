367a18fda79648dd55ed959cb073bc4aaf4cacf1
Jeff Schwab <jeff@unbuggy.com>
2021-12-31 09:47:21 -0500

    Move cargo related code to submodule

--- cargo-norm/tests/a-cargo-binary.rs
+++ cargo-norm/tests/a-cargo-binary.rs
@@ -1,41 +1,11 @@
-use std::error::Error;
-use std::fmt::{self, Display, Formatter};
-use std::io;
+mod cargo;
+
 use std::process::Command;
 
 const BINARY: &str = "../target/debug/cargo-norm";
 
-#[derive(Debug)]
-struct CargoError {
-    what: String,
-}
-
-impl Display for CargoError {
-    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
-        write!(f, "{}", self.what)
-    }
-}
-
-impl Error for CargoError {}
-
-impl From<io::Error> for CargoError {
-    fn from(error: io::Error) -> Self {
-        CargoError {
-            what: error.to_string(),
-        }
-    }
-}
-
-fn build() -> Result<(), CargoError> {
-    if !Command::new("cargo").arg("build").status()?.success() {
-        let what = "cargo build returned bad status".to_string();
-        return Err(CargoError { what });
-    }
-    Ok(())
-}
-
 #[test]
 fn runs() {
-    build().expect("can't build");
+    cargo::build().expect("can't build");
     Command::new(BINARY).status().expect("can't run");
 }
