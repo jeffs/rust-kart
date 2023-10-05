2e57a85ac3d518d40444749e7c234bad512a7966
Jeff Schwab <jeff@unbuggy.com>
2023-10-05 12:34:32 -0400

    Update git-prune

--- git-prune/src/main.rs
+++ git-prune/src/main.rs
@@ -1,185 +1,85 @@
-//! This program deletes Git branches (local and on remote 'origin') that are
-//! not ahead of the main branch (or master branch, if main doesn't exist).
-//!
-//! Sample usage:
-//!
-//! ```sh
-//! $ git-prune -n
-//! jeff.git-prune
-//! origin/jeff.git-prune
-//!
-//! $ git-prune
-//! deleting jeff.git-prune
-//! deleting origin/jeff.git-prune
-//! ```
+//! This program pulls the main branch of the repo from which it's called, then
+//! deletes any local branches that are not ahead of main, and finally checks
+//! back out the original branch.  The main branch defaults to `main`, but
+//! `master` is used as a fallback if no `main` branch is found.
 
 use std::env;
-use std::mem::take;
-use std::process::{exit, Command};
+use std::error::Error;
+use std::ffi::OsStr;
+use std::process::exit;
+use tokio::process::Command;
 
-const MAIN_REFS: [&str; 2] = ["main", "master"];
-
-/// Runs git with the specified args, then trims and returns its output lines.
-fn git<'a, I>(args: I) -> Result<Vec<String>, String>
+async fn git<S, I>(args: I) -> Result<String, String>
 where
-    I: IntoIterator<Item = &'a str>,
+    I: IntoIterator<Item = S>,
+    S: AsRef<OsStr>,
 {
     let output = Command::new("git")
-        .args(args.into_iter())
+        .args(args)
         .output()
-        .map_err(|e| format!("can't run git: {e}"))?;
+        .await
+        .expect("git should be executable");
+    let stderr = String::from_utf8_lossy(&output.stderr);
     if !output.status.success() {
-        return Err(String::from_utf8_lossy(&output.stderr).into());
+        return Err(stderr.into());
     }
-    Ok(String::from_utf8_lossy(&output.stdout)
+    let stdout = String::from_utf8_lossy(&output.stdout);
+    Ok((stderr + stdout).into())
+}
+
+/// Returns the local main branch (trunk) name, or an error.
+async fn trunk() -> Result<&'static str, String> {
+    let names = ["main", "master"];
+    for branch in names {
+        if git(["show-ref", branch]).await.is_ok() {
+            return Ok(branch);
+        }
+    }
+    let names = names.join(" or ");
+    Err(format!("expected trunk; can't find {names}"))
+}
+
+async fn main_imp() -> Result<(), Box<dyn Error>> {
+    // Check out the main branch.
+    git(["checkout", trunk().await?]).await?;
+    let _ = git(["pull", "--prune"]).await;
+
+    // List all branches except the one we just checked out.
+    let branches = git(["branch"]).await?;
+    let branches = branches
         .lines()
-        .map(|s| s.trim().to_string())
-        .collect())
-}
+        .filter(|line| !line.starts_with('*'))
+        .map(|line| line.trim());
 
-/// Passes args to git() and returns the first output line.  Returns an error
-/// unless git outputs exactly one line.
-fn git_single<'a, I>(args: I) -> Result<String, String>
-where
-    I: IntoIterator<Item = &'a str>,
-{
-    let mut lines = git(args)?;
-    match lines.pop() {
-        Some(line) if lines.is_empty() => Ok(line),
-        _ => Err("expected exactly one line from git".to_string()),
-    }
-}
-
-/// Temporarily moves HEAD to a supplied target branch, then restores HEAD to
-/// its original (source) branch on drop.  Cheaper than actually checking out
-/// the target branch.
-struct SymRef {
-    source: String,
-}
-
-impl SymRef {
-    fn target(target: &str) -> Result<SymRef, String> {
-        let source = git_single(["symbolic-ref", "HEAD"])?;
-        git(["symbolic-ref", "-m", "git-prune push", "HEAD", target])?;
-        Ok(SymRef { source })
-    }
-}
-
-impl Drop for SymRef {
-    fn drop(&mut self) {
-        if let Err(why) = git(["symbolic-ref", "-m", "git-prune pop", "HEAD", &self.source]) {
-            eprintln!("warning: can't restore HEAD to original ref: {why}");
-        }
-    }
-}
-
-fn full_name(branch: &str) -> Result<String, String> {
-    git_single(["rev-parse", "--symbolic-full-name", branch])
-}
-
-fn fetch_main_ref() -> Result<String, &'static str> {
-    for branch in MAIN_REFS {
-        if let Ok(sym) = full_name(&format!("origin/{branch}")) {
-            if git(["fetch", "origin", branch]).is_err() {
-                eprintln!("warning: can't fetch origin {branch}");
-            }
-            return Ok(sym);
-        }
-        if let Ok(sym) = full_name(branch) {
-            return Ok(sym);
-        }
-    }
-    Err("no main or master branch found")
-}
-
-fn strip_stars(mut lines: Vec<String>) -> Vec<String> {
-    for line in &mut lines {
-        if let Some(suffix) = line.strip_prefix("* ") {
-            let branch = suffix.to_string();
-            *line = branch;
-        }
-    }
-    lines
-}
-
-fn list_local_branches() -> Result<Vec<String>, String> {
-    Ok(strip_stars(git(["branch"])?))
-}
-
-fn list_remote_branches() -> Result<Vec<String>, String> {
-    let mut branches = strip_stars(git(["branch", "--remotes"])?);
-    branches.retain(|s| s.starts_with("origin/") && !s.contains(' '));
-    Ok(branches)
-}
-
-fn is_main(branch: &str) -> bool {
-    if let Some(suffix) = branch.strip_prefix("origin/") {
-        MAIN_REFS.contains(&suffix)
-    } else {
-        MAIN_REFS.contains(&branch.as_ref())
-    }
-}
-
-fn is_empty(branch: &str) -> Result<bool, String> {
-    Ok(git(["log", &format!("..{branch}")])?.is_empty())
-}
-
-fn is_obsolete(branch: &str) -> Result<bool, String> {
-    Ok(!is_main(&branch) && is_empty(&branch)?)
-}
-
-fn retain_obsolete(mut branches: Vec<String>) -> Result<Vec<String>, String> {
-    let mut target = 0;
-    for source in 0..branches.len() {
-        let mut branch: String = take(&mut branches[source]);
-        if is_obsolete(&branch)? {
-            branches[target] = take(&mut branch);
-            target += 1;
-        }
-    }
-    branches.truncate(target);
-    Ok(branches)
-}
-
-fn for_each_obsolete_branch(
-    handle_branch: &dyn Fn(&str) -> Result<(), String>,
-) -> Result<(), String> {
-    let _ = git(["remote", "prune", "origin"]);
-    let mut branches = list_local_branches()?;
-    branches.append(&mut list_remote_branches()?);
-    {
-        let _drop_symref = SymRef::target(&fetch_main_ref()?)?;
-        branches = retain_obsolete(branches)?;
-    }
+    // List branches that are not ahead of main.
+    let mut dead_branches = Vec::new();
     for branch in branches {
-        handle_branch(&branch)?;
+        let range = format!("..{branch}");
+        if git(["rev-list", "--count", &range]).await? == "0\n" {
+            dead_branches.push(branch);
+        }
     }
-    Ok(())
-}
 
-fn delete_branch(branch: &str) -> Result<(), String> {
-    eprintln!("deleting {branch}");
-    if let Some(suffix) = branch.strip_prefix("origin/") {
-        git(["push", "origin", &format!(":{suffix}")])?;
-    } else {
-        git(["branch", "-d", &branch])?;
+    // Delete branches that are not ahead of main.
+    if !dead_branches.is_empty() {
+        let list = dead_branches.join(", ");
+        git(["branch", "-d"].into_iter().chain(dead_branches)).await?;
+        eprintln!("Deleted {list}");
     }
+
+    // Return to the originally checked out branch, unless it's gone.
+    let _ = git(["checkout", "-"]).await;
     Ok(())
 }
 
-fn print_branch(branch: &str) -> Result<(), String> {
-    println!("{branch}");
-    Ok(())
-}
-
-fn main() {
-    let handle_branch = if env::args().skip(1).any(|arg| arg == "-n") {
-        print_branch
-    } else {
-        delete_branch
-    };
-    if let Err(what) = for_each_obsolete_branch(&handle_branch) {
-        eprintln!("{what}");
+#[tokio::main]
+async fn main() {
+    if env::args().len() > 1 {
+        eprintln!("git-prune: error: expected empty argument list");
+        exit(2);
+    }
+    if let Err(err) = main_imp().await {
+        eprintln!("git-prune: {err}");
         exit(1);
     }
 }
