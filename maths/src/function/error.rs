f1297e06ba6d0a4e03e0d7f3f3ac3550167216f2
Jeff Schwab <jeff@unbuggy.com>
2023-11-06 11:53:11 -0500

    Factor maths into smaller, purer functions

--- maths/src/function/error.rs
+++ maths/src/function/error.rs
@@ -6,7 +6,7 @@ pub struct BadFunc(pub String);
 impl Display for BadFunc {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let BadFunc(arg) = self;
-        write!(f, "{arg}: bad function")
+        write!(f, "{arg}: bad function; expected 'add' or 'mul'")
     }
 }
 
--- maths/src/main.rs
+++ maths/src/main.rs
@@ -1,46 +1,58 @@
-use std::{env, error::Error, io::stdin, process};
+use std::{env, error::Error, io::stdin, num::ParseFloatError, process};
 
 use crate::function::{error::NoFunc, Function};
 
 mod function;
 
-fn apply_args<I: IntoIterator<Item = String>>(
-    func: &dyn Function,
-    args: I,
-) -> Result<f64, Box<dyn Error>> {
-    let mut acc: f64 = func.identity();
-    for arg in args {
-        let value: f64 = arg.parse()?;
-        acc = func.apply(acc, value);
-    }
-    Ok(acc)
+type BoxedError = Box<dyn Error>;
+
+fn apply_args(func: &dyn Function, first: f64, rest: &[f64]) -> f64 {
+    rest.into_iter().fold(first, |x, &y| func.apply(x, y))
 }
 
-fn apply_stdin(func: &dyn Function) -> Result<(), Box<dyn Error>> {
-    let sym = func.symbol();
-    let mut acc: f64 = func.identity();
-    println!("      {acc}");
-    for line in stdin().lines() {
-        let line = line?;
-        for word in line.split_ascii_whitespace() {
-            let value: f64 = word.parse()?;
-            acc = func.apply(acc, value);
-            println!("    {sym} {value} = {acc}");
-        }
-    }
+fn parse_line(line: &str) -> Result<impl Iterator<Item = f64>, ParseFloatError> {
+    let values: Result<Vec<f64>, _> = line
+        .split_ascii_whitespace()
+        .map(|word| word.parse())
+        .collect();
+    Ok(values?.into_iter())
+}
+
+fn apply_line<I: Iterator<Item = f64>>(func: &dyn Function, args: I) -> f64 {
+    let value = args.fold(func.identity(), |x, y| func.apply(x, y));
+    eprintln!("{} {value}", func.symbol());
+    value
+}
+
+fn apply_stdin(func: &dyn Function) -> Result<f64, BoxedError> {
+    // I wish Rust had Scala-style comprehensions.
+    stdin()
+        .lines()
+        .map(|result| -> Result<String, BoxedError> { Ok(result?) }) // Box io::Error
+        .map(|result| result.and_then(|line| Ok(parse_line(&line)?))) // Parse line into Vec<f64>
+        .map(|result| result.map(|args| apply_line(func, args))) // Apply monoid to each line
+        .reduce(|result, line| Ok(func.apply(result?, line?))) // Apply monoid to per-line results
+        .unwrap_or(Ok(func.identity())) // Default to monoid identity if stdin was empty
+}
+
+fn parse_args() -> Result<(&'static dyn Function, Vec<f64>), BoxedError> {
+    let mut args = env::args().skip(1);
+    let func = args.next().ok_or(NoFunc)?;
+    let func = function::parse(&func)?;
+    let args: Result<Vec<f64>, _> = args.map(|arg| arg.parse()).collect();
+    let args = args?;
+    Ok((func, args))
+}
+
+/// Applies the passed monoid to the rest of the args if any, or else to stdin.
+fn main_imp() -> Result<(), BoxedError> {
+    let (func, args) = parse_args()?;
+    let answer = match args.as_slice() {
+        [first, rest @ ..] => apply_args(func, *first, rest),
+        _ => apply_stdin(func)?,
+    };
+    println!("= {answer}");
     Ok(())
-}
-
-fn main_imp() -> Result<(), Box<dyn Error>> {
-    let mut args = env::args().skip(1).peekable();
-    let arg = args.next().ok_or(NoFunc)?;
-    let func = function::parse(&arg)?;
-    if args.peek().is_none() {
-        apply_stdin(func)
-    } else {
-        println!("{}", apply_args(func, args)?);
-        Ok(())
-    }
 }
 
 fn main() {
