5565128ecd54098cad982dfe4da82e9a0a059c2e
Jeff Schwab <jeff@unbuggy.com>
2023-11-13 07:47:31 -0500

    Format sum-of-products output

--- sum-of-products/src/bin/sop.rs
+++ sum-of-products/src/bin/sop.rs
@@ -1,21 +1,36 @@
 use std::{error::Error, io::stdin};
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let products: Result<Vec<f64>, _> = stdin()
+    let parsed = stdin()
         .lines()
-        .map(|line| -> Result<f64, Box<dyn Error>> {
-            let numbers: Result<Vec<f64>, _> = line?
+        .map(|line| -> Result<_, Box<dyn Error>> {
+            let values: Result<Vec<f64>, _> = line?
                 .split_ascii_whitespace()
                 .map(|word| word.parse())
                 .collect();
-            Ok(numbers?.into_iter().product())
+            Ok(values?)
         })
+        .collect::<Result<Vec<Vec<f64>>, _>>()?;
+    let products: Vec<f64> = parsed
+        .iter()
+        .map(|values| values.iter().product::<f64>())
         .collect();
-    let products = products?;
     let sum: f64 = products.iter().sum();
 
-    for product in products {
-        println!("{product:>8}");
+    let formulas = parsed.into_iter().map(|values| {
+        let columns: Vec<String> = values
+            .into_iter()
+            .map(|value| format!("{value:>3}"))
+            .collect();
+        columns.join("  * ")
+    });
+
+    let mut pairs = products.into_iter().zip(formulas.into_iter());
+    if let Some((product, formula)) = pairs.next() {
+        println!("{product:>8}   = {formula:>18}");
+        for (product, formula) in pairs {
+            println!("+ {product:>6}   = {formula:>18}");
+        }
     }
     println!("--------");
     println!("{sum:>8}");
