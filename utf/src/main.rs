eda07d56f0a8fd4d801ecf1b780f0d94c36ebf15
Jeff Schwab <jeff@unbuggy.com>
2024-03-04 12:01:37 -0500

    Add program to print emoji from codepoints

---
+++ utf/src/main.rs
@@ -0,0 +1,53 @@
+use std::{env, error, fmt::Display, process};
+
+#[derive(Debug)]
+struct BadCodepoint(String);
+
+impl BadCodepoint {
+    pub fn from_arg(arg: &str) -> BadCodepoint {
+        BadCodepoint(format!("{arg}: bad codepoint"))
+    }
+}
+
+impl Display for BadCodepoint {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl error::Error for BadCodepoint {}
+
+fn parse_codepoint(arg: &str) -> Result<char, BadCodepoint> {
+    let Ok(codepoint) = u32::from_str_radix(&arg, 16) else {
+        return Err(BadCodepoint::from_arg(arg));
+    };
+    Ok(char::from_u32(codepoint).ok_or_else(|| BadCodepoint::from_arg(arg))?)
+}
+
+fn parse_arg(arg: &str) -> Result<char, BadCodepoint> {
+    match arg {
+        s if s.starts_with("poo") => Ok('\u{1f4a9}'), // 💩
+        "horns" => Ok('\u{1f918}'),                   // 🤘
+        _ => parse_codepoint(arg),
+    }
+}
+
+fn main() {
+    let args: Result<Vec<char>, _> = env::args().skip(1).map(|arg| parse_arg(&arg)).collect();
+    match args {
+        Ok(chars) if chars.is_empty() => {
+            eprintln!("utf: error: expected codepoints");
+            process::exit(2);
+        }
+        Ok(chars) => {
+            for c in chars {
+                print!("{c}");
+            }
+            println!();
+        }
+        Err(err) => {
+            eprintln!("utf: error: {err}");
+            process::exit(2);
+        }
+    }
+}
