1dafddd1c27381fd82ad953cddac20968ebcf35d
Jeff Schwab <jeff@unbuggy.com>
2024-04-18 08:46:24 -0400

    Add egg whites

--- kcal/src/food.rs
+++ kcal/src/food.rs
@@ -40,6 +40,7 @@ const FOODS: &[(&str, FoodDescriptor)] = &[
     food!(cauliflower, 25,  1.9, 100, "Cauliflower, raw",                             "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169986/nutrients"),
     food!(celery,      14,  0.7, 100, "Celery, raw",                                  "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169988/nutrients"),
     food!(chicken,     60, 11.0,  56, "Blazing Buffalo Style Roasted Chicken Breast", "https://boarshead.com/products/detail/440-blazing-buffalo-style-roasted-chicken-breast"),
+    food!(eggwhite,    25,  5.0,  46, "Egg whites",                                   "carton"),
     food!(endive,      17,  1.3, 100, "Endive",                                       "https://fdc.nal.usda.gov/fdc-app.html#/food-details/168412/nutrients"),
     food!(enoki,       44,  2.4, 100, "Mushroom, enoki",                              "https://fdc.nal.usda.gov/fdc-app.html#/food-details/2003600/nutrients"),
     food!(pepper,      20,  0.9, 100, "Peppers, sweet, green, raw",                   "https://fdc.nal.usda.gov/fdc-app.html#/food-details/170427/nutrients"),
