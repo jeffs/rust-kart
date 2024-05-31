4871b35cbced360a82841294c04954e8b7148af7
Jeff Schwab <jeff@unbuggy.com>
2024-05-31 19:58:41 -0400

    Fix script, I hope

--- scripts/doc.zsh
+++ scripts/doc.zsh
@@ -3,7 +3,8 @@
 cd "$(dirname "$(dirname "$0")")"
 
 rm -rf target/doc
-cargo doc --document-private-items --no-deps
+RUSTDOCFLAGS='--enable-index-page -Zunstable-options' \
+    cargo +nightly doc --document-private-items --no-deps
 
 rm -rf docs
 mv target/doc docs
