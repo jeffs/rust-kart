7f0e41307f56e462879da85b285bbfe46b3c5c65
Jeff Schwab <jeff@unbuggy.com>
2024-05-31 20:44:49 -0400

    Flesh out pangram command doc

--- pangram/src/main.rs
+++ pangram/src/main.rs
@@ -1,6 +1,24 @@
+//! NY Times Spelling Bee solver.
+//!
 //! # Usage
 //! ```sh
-//! pangram <letters> [words-file]
+//! pangram LETTERS [WORDS_FILE]
+//! ```
+//!
+//! Any uppercase LETTERS are considered mandatory.  Each solution is printed on
+//! a separate line, and pangrams (that is, words that use all available
+//! LETTERS) are prefixed with asterisks.
+//!
+//! The default WORDS_FILE is /usr/share/dict/words.  Note that this file may
+//! not match the New York Times' word list.  In particular, it may contain
+//! proper nouns, which you can grep out, as in the following example.
+//!
+//! # Example
+//! ```sh
+//! $ pangram Eachkmn |rg -v '[A-Z]' |tail -3
+//!   nankeen
+//! * checkman
+//!   henchman
 //! ```
 //!
 //! # To Do
