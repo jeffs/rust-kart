05924c7a8745f6d69dc68ec681a963f428a388dd
Jeff Schwab <jeff@unbuggy.com>
2024-06-24 05:43:46 -0400

    Delegate citations to CSV

--- kcal/src/food.rs
+++ kcal/src/food.rs
@@ -12,8 +12,6 @@ impl Display for BadFood {
 
 #[derive(Clone)]
 pub struct Food {
-    pub description: &'static str,
-    pub source: &'static str,
     /// kilocalories per 100g of this food
     pub kcal: f64,
     /// grams of protein per 100g of this food
@@ -21,43 +19,44 @@ pub struct Food {
 }
 
 macro_rules! food {
-    ($name: ident, $kcal: expr, $protein: expr, $per: expr, $description: expr, $source: expr) => {
+    ($name: ident, $kcal: expr, $protein: expr, $per: expr) => {
         (
             stringify!($name),
             Food {
-                description: $description,
                 kcal: $kcal as f64 * 100.0 / $per as f64,
                 protein: $protein as f64 * 100.0 / $per as f64,
-                source: $source,
             },
         )
     };
 }
 
+// TODO: Move to food.csv.
 #[rustfmt::skip]
 const FOODS: &[(&str, Food)] = &[
-    food!(broccoli,    34,  2.8, 100, "Broccoli, raw",                                "https://fdc.nal.usda.gov/fdc-app.html#/food-details/170379/nutrients"),
-    food!(cabbage,     25,  1.3, 100, "Cabbage, raw",                                 "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169975/nutrients"),
-    food!(carrot,      41,  0.8, 100, "Carrots, baby, raw",                           "https://fdc.nal.usda.gov/fdc-app.html#/food-details/2258587/nutrients"),
-    food!(cauliflower, 25,  1.9, 100, "Cauliflower, raw",                             "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169986/nutrients"),
-    food!(celery,      14,  0.7, 100, "Celery, raw",                                  "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169988/nutrients"),
-    food!(chicken,     60, 11.0,  56, "Blazing Buffalo Style Roasted Chicken Breast", "https://boarshead.com/products/detail/440-blazing-buffalo-style-roasted-chicken-breast"),
-    food!(cucumber,    15,  0.6, 100, "Cucumber, with peel, raw",                     "https://fdc.nal.usda.gov/fdc-app.html#/food-details/168409/nutrients"),
-    food!(eggwhite,    25,  5.0,  46, "Egg whites",                                   "label"),
-    food!(endive,      17,  1.3, 100, "Endive",                                       "https://fdc.nal.usda.gov/fdc-app.html#/food-details/168412/nutrients"),
-    food!(enoki,       44,  2.4, 100, "Mushroom, enoki",                              "https://fdc.nal.usda.gov/fdc-app.html#/food-details/2003600/nutrients"),
-    food!(mushroom,    22,  3.1, 100, "Mushrooms, white, raw",                        "https://fdc.nal.usda.gov/fdc-app.html#/food-details/169251/nutrients"),
-    food!(oil,        884,  0.0, 100, "Oil, olive, salad or cooking",                 "https://fdc.nal.usda.gov/fdc-app.html#/food-details/171413/nutrients"),
-    food!(onion,       41,  1.3, 100, "Red Onion",                                    "https://www.nutritionix.com/food/red-onion"),
-    food!(pepper,      20,  0.9, 100, "Peppers, sweet, green, raw",                   "https://fdc.nal.usda.gov/fdc-app.html#/food-details/170427/nutrients"),
-    food!(popcorn,    100,  4.0,  27, "Popcorn",                                      "label"),
-    food!(popcorn_usda, 387, 12.9, 100, "Snacks, popcorn, air-popped",                  "https://fdc.nal.usda.gov/fdc-app.html#/food-details/167959/nutrients"),
-    food!(spinach,     23,  2.9, 100, "Spinach, raw",                                 "https://fdc.nal.usda.gov/fdc-app.html#/food-details/168462/nutrients"),
-    food!(thigh,      149, 18.6, 100, "Chicken, thigh, boneless, skinless, raw",      "https://fdc.nal.usda.gov/fdc-app.html#/food-details/2646171/nutrients"),
-    food!(tomato,      22,  0.7, 100, "Tomato, roma",                                 "https://fdc.nal.usda.gov/fdc-app.html#/food-details/1999634/nutrients"),
-    food!(turkey,      64,  7.7,  57, "Deli Sliced Turkey",                           "https://www.nutritionix.com/food/deli-sliced-turkey"),
-    food!(veg,         20,  1.5,  57, "Mixed vegetables",                             "rough average of squash, mushrooms, and asparagus"),
-    food!(whiskey,    250,  0.0, 100, "Whisky",                                       "Google"),
+    food!(broccoli,        34,  2.8, 100),
+    food!(brussels,        43,  3.4, 100),
+    food!(cabbage,         25,  1.3, 100),
+    food!(carrot,          41,  0.8, 100),
+    food!(cauliflower,     25,  1.9, 100),
+    food!(celery,          14,  0.7, 100),
+    food!(chicken,         60, 11.0,  56),
+    food!(cucumber,        15,  0.6, 100),
+    food!(eggwhite,        25,  5.0,  46),
+    food!(endive,          17,  1.3, 100),
+    food!(enoki,           44,  2.4, 100),
+    food!(lettuce,         17,  1.0, 100),
+    food!(mushroom,        22,  3.1, 100),
+    food!(oil,            884,  0.0, 100),
+    food!(onion,           41,  1.3, 100),
+    food!(pepper,          20,  0.9, 100),
+    food!(popcorn,        100,  4.0,  27),
+    food!(popcorn_usda,   387, 12.9, 100),
+    food!(spinach,         23,  2.9, 100),
+    food!(thigh,          149, 18.6, 100),
+    food!(tomato,          22,  0.7, 100),
+    food!(turkey,          64,  7.7,  57),
+    food!(veg,             20,  1.5,  57),
+    food!(whiskey,        250,  0.0, 100),
 ];
 
 /// Parses foods in the format C,P/Z.
@@ -79,8 +78,6 @@ fn parse_custom(s: &str) -> Option<Food> {
         / 100.0;
 
     Some(Food {
-        description: "ad hoc",
-        source: "user",
         kcal: kcal / hundreds,
         protein: protein / hundreds,
     })
