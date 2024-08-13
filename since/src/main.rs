9b85d802ea5045cfc3668527295d86a2c79a58d7
Jeff Schwab <jeff@unbuggy.com>
2024-08-13 13:10:49 -0400

    Add 'since' program

---
+++ since/src/main.rs
@@ -0,0 +1,27 @@
+use std::{
+    env,
+    os::unix::process::CommandExt,
+    process::{exit, Command},
+};
+
+fn main() {
+    let mut base: Option<&str> = None;
+    let mut flags: Vec<&str> = Vec::new();
+    let our_args: Vec<String> = env::args().skip(1).collect();
+    for arg in our_args.iter() {
+        if arg.starts_with("-") {
+            flags.push(arg);
+        } else if base.is_none() {
+            base = Some(arg);
+        } else {
+            eprintln!("{arg}: unexpected argument");
+            exit(2);
+        }
+    }
+
+    Command::new("git")
+        .args(["log", "--first-parent", "--oneline"])
+        .args(flags)
+        .arg(format!("{}..", base.unwrap_or("main")))
+        .exec();
+}
