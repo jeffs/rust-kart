35d3bedf30161500ab730fa93beb95f917754bc5
Jeff Schwab <jeff@unbuggy.com>
2024-09-26 12:49:36 -0400

    Add module-from-path

---
+++ module-from-path/src/main.rs
@@ -0,0 +1,43 @@
+use clap::Parser;
+use std::env::current_dir;
+use std::path::{Path, PathBuf};
+use std::process::exit;
+
+type Error = Box<dyn std::error::Error>;
+type Result<T> = std::result::Result<T, Error>;
+
+#[derive(Debug, Parser)]
+struct Args {
+    path: PathBuf,
+
+    #[arg(default_value_t = String::from("::"))]
+    sep: String,
+}
+
+fn normalize(path: &Path) -> Result<PathBuf> {
+    let path = path.with_extension("");
+    Ok(if path.is_absolute() {
+        path.strip_prefix(current_dir()?)?.to_owned()
+    } else {
+        path
+    })
+}
+
+fn main_imp() -> Result<()> {
+    let args = Args::parse();
+    let path = normalize(&args.path)?;
+    let parts: Vec<_> = path
+        .components()
+        .filter_map(|p| p.as_os_str().to_str())
+        .collect();
+    let module = parts.join(&args.sep);
+    println!("{module}");
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = main_imp() {
+        eprintln!("Error: {err}");
+        exit(1);
+    }
+}
