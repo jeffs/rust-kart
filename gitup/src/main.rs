64d5270413aa561eccee62129fad72a26106da81
Jeff Schwab <jeff@unbuggy.com>
2025-01-13 11:43:23 -0500

    Let gitup remove branches whose upstreams are gone

--- gitup/src/main.rs
+++ gitup/src/main.rs
@@ -1,44 +1,85 @@
-//! This program pulls the main branch of the repo from which it's called, then
-//! deletes any local branches that are not ahead of main, and finally checks
-//! back out the original branch.  The main branch defaults to `main`, but
-//! `master` is used as a fallback if no `main` branch is found.
+//! This program pulls the main branch of the repo from which it's called, then deletes any local
+//! branches that are not ahead of main, and finally checks back out the original branch.  The main
+//! branch defaults to `main`, but `master` is used as a fallback if no `main` branch is found.
 //!
 //! # TODO
 //!
 //! * [ ] Delete remote branches behind trunk.
-//! * [ ] Don't mess up "checkout -" by checking out main.  I tried _not_
-//!   checking out main, but after fetch --prune, the user still sees a list of
-//!   obsolete branches the next time they pull --prune; so now this program
-//!   checks out main just so it can run pull --prune per se.
+//! * [ ] Don't mess up "checkout -" by checking out main.  I tried _not_ checking out main, but
+//!   after fetch --prune, the user still sees a list of obsolete branches the next time they pull
+//!   --prune; so now this program checks out main just so it can run pull --prune per se.
 //! * [ ] Support squashed merges.
 
 use std::collections::HashSet;
-use std::error::Error;
 use std::ffi::OsStr;
+use std::io::Write;
 use std::path::Path;
 use std::process::{exit, ExitStatus};
-use std::{env, fmt};
+use std::{env, fmt, io};
 use tokio::process::Command;
 
+/// Possible names of local trunk branches, in order of preference.
+const TRUNKS: [&str; 2] = ["main", "master"];
+
 #[derive(Debug)]
-struct SimpleError(String);
+enum Error {
+    /// An unrecognized command line argument was supplied.
+    Arg(String),
 
-impl fmt::Display for SimpleError {
+    /// Git returned bad status, and printed the supplied text to standard error.
+    Git(String),
+
+    /// No local trunk branch could be identified.
+    Trunk,
+
+    /// The Git working copy has uncommitted changes.
+    Unclean,
+}
+
+impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.0)
+        match self {
+            Error::Arg(arg) => write!(f, "{arg:?}: unexpected argument"),
+            Error::Git(stderr) => write!(f, "{stderr}"),
+            Error::Trunk => write!(f, "can't find local trunk; any of {TRUNKS:?}"),
+            Error::Unclean => write!(f, "working copy is unclean"),
+        }
     }
 }
 
-impl<S> From<S> for SimpleError
-where
-    S: Into<String>,
-{
-    fn from(value: S) -> Self {
-        SimpleError(value.into())
-    }
+type Result<T> = std::result::Result<T, Error>;
+
+/// Policy for which branches to delete.
+#[allow(dead_code)]
+struct Args {
+    /// Specifies removal of local branches merged to local trunk.  Note that this does not include
+    /// GitHub "squash merges," which do not actually merge the original branch.
+    merged: bool,
+
+    /// Specifies removal of local branches whose upstreams are gone.  Upstream branches are often
+    /// deleted after being merged to trunk, even if they were "squash merged," so this is a useful
+    /// way to detect such "merges."
+    gone: bool,
 }
 
-impl Error for SimpleError {}
+impl Args {
+    /// Returns the name of this program for use in error logs, and the branch removal policy.
+    fn from_env() -> Result<Args> {
+        let mut args = env::args().skip(1);
+
+        if let Some(arg) = args.next() {
+            return Err(Error::Arg(arg));
+        }
+
+        // TODO: Set branch removal policy from CLI.
+        let args = Args {
+            merged: true,
+            gone: true,
+        };
+
+        Ok(args)
+    }
+}
 
 fn format_git_command<S, I>(args: I) -> String
 where
@@ -69,7 +110,7 @@ where
 }
 
 #[allow(dead_code)]
-async fn git_loud<S, I>(args: I) -> Result<String, SimpleError>
+async fn git_loud<S, I>(args: I) -> Result<String>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
@@ -79,7 +120,7 @@ where
 
     let (status, stdout, stderr) = run_git(args).await;
     if !status.success() {
-        return Err(stderr.into());
+        return Err(Error::Git(stderr));
     }
 
     let lines: Vec<_> = stdout.lines().collect();
@@ -97,28 +138,36 @@ where
     Ok(stderr + &stdout)
 }
 
-async fn git<S, I>(args: I) -> Result<String, SimpleError>
+async fn git<S, I>(args: I) -> Result<String>
 where
     I: IntoIterator<Item = S>,
     S: AsRef<OsStr>,
 {
     let (status, stdout, stderr) = run_git(args).await;
     if !status.success() {
-        return Err(stderr.into());
+        return Err(Error::Git(stderr));
     }
     Ok(stderr + &stdout)
 }
 
+/// Splits the specified Git output into lines, excludes any line beginning with "*", and trims
+/// leading whitespace from each line.  Note that the output is not necessarily a list of simple
+/// branch names; e.g., if the output is from `git branch --verbose`.
+fn trim_branches(stdout: &str) -> impl Iterator<Item = &str> {
+    stdout
+        .lines()
+        .filter(|line| !line.starts_with("* "))
+        .map(str::trim_ascii_start)
+}
+
 /// Returns the local main branch (trunk) name, or an error.
-async fn local_trunk() -> Result<&'static str, String> {
-    let names = ["main", "master"];
-    for branch in names {
+async fn local_trunk() -> Result<&'static str> {
+    for branch in TRUNKS {
         if git(["show-ref", branch]).await.is_ok() {
             return Ok(branch);
         }
     }
-    let names = names.join(" or ");
-    Err(format!("expected trunk; can't find {names}"))
+    Err(Error::Trunk)
 }
 
 async fn upstream(branch: &str) -> Option<String> {
@@ -133,13 +182,15 @@ async fn upstream(branch: &str) -> Option<String> {
     .map(|s| s.trim().to_owned())
 }
 
-async fn is_working_copy_clean() -> Result<bool, SimpleError> {
+async fn is_working_copy_clean() -> Result<bool> {
     Ok(git(["status", "--porcelain"]).await?.is_empty())
 }
 
-async fn main_imp() -> Result<(), SimpleError> {
+async fn main_imp() -> Result<()> {
+    let rm = Args::from_env()?; // Branch removal policy.
+
     if !is_working_copy_clean().await? {
-        return Err(SimpleError("working copy is unclean".to_owned()));
+        return Err(Error::Unclean);
     }
 
     let orig = git(["rev-parse", "--abbrev-ref", "HEAD"]).await?;
@@ -157,14 +208,21 @@ async fn main_imp() -> Result<(), SimpleError> {
         }
     }
 
-    let branches = git(["branch", "--merged"]).await?;
-    let dead_branches = branches
-        .lines()
-        .filter(|line| !line.starts_with("* "))
-        .map(str::trim_ascii_start)
-        .collect::<HashSet<_>>();
+    let mut dead_branches = HashSet::<String>::new();
+    if rm.merged {
+        dead_branches.extend(trim_branches(&git(["branch", "--merged"]).await?).map(str::to_owned));
+    }
 
-    if dead_branches.contains(&orig) {
+    if rm.gone {
+        dead_branches.extend(
+            trim_branches(&git(["branch", "--list", "--verbose"]).await?)
+                .filter(|line| line.contains("[gone]"))
+                .flat_map(|line| line.split_ascii_whitespace().next())
+                .map(str::to_owned),
+        );
+    }
+
+    if dead_branches.contains(orig) {
         // Let the user know we're not leaving HEAD on the original branch.
         println!("co {trunk}");
     } else if trunk != orig {
@@ -175,7 +233,10 @@ async fn main_imp() -> Result<(), SimpleError> {
         for zombie in &dead_branches {
             println!("rm {zombie}");
         }
-        git(["branch", "-D"].into_iter().chain(dead_branches)).await?;
+        git(["branch", "-D"]
+            .into_iter()
+            .chain(dead_branches.iter().map(String::as_str)))
+        .await?;
     }
 
     Ok(())
@@ -184,21 +245,22 @@ async fn main_imp() -> Result<(), SimpleError> {
 #[tokio::main]
 async fn main() {
     let mut args = env::args_os();
+
     let name = args
         .next()
         .expect("argv[0] should hold the path to this executable");
     let name: &Path = name.as_ref();
     let name = name
         .file_stem()
-        .expect("executable path should terminate in file name")
-        .to_string_lossy();
+        .expect("executable path should terminate in file name");
 
-    if let Some(arg) = args.next() {
-        eprintln!("{name}: error: {arg:?}: unexpected argument");
-        exit(2);
-    }
     if let Err(err) = main_imp().await {
-        eprintln!("{name}: error: {err}");
+        // [`OsStr::display`] is not yet stable:
+        // <https://doc.rust-lang.org/std/ffi/struct.OsStr.html#method.display>
+        io::stderr()
+            .write(name.as_encoded_bytes())
+            .expect("stderr should be writable");
+        eprintln!(": error: {err}");
         exit(1);
     }
 }
