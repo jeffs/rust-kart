2e1ef341bde7766f63d8e74f9d49efb4290f8cd8
Jeff Schwab <jeff@unbuggy.com>
2025-02-07 15:25:34 -0500

    Types over variants

--- arg5-2/src/lib.rs
+++ arg5-2/src/lib.rs
@@ -4,32 +4,21 @@ use std::ffi::OsString;
 use std::fmt;
 
 #[derive(Debug, PartialEq)]
-enum FlagName {
-    Short(char),
-    Long(&'static str),
-}
-
-impl fmt::Display for FlagName {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match self {
-            FlagName::Short(c) => write!(f, "{c}"),
-            FlagName::Long(w) => write!(f, "{w}"),
-        }
-    }
-}
-
-#[derive(Debug, PartialEq)]
 enum Error {
     /// The target variable for a Boolean flag was already true when the variable was registered.
     /// The variable should have been initialized to false, or else there is no way to tell whether
     /// the flag was specified in arguments.
-    Tautology(FlagName),
+    CharTautology(char),
+    LongTautology(&'static str),
+    FlexTautology(char, &'static str),
 }
 
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            Error::Tautology(name) => write!(f, "flag '{name}' would always be true"),
+            Error::CharTautology(c) => write!(f, "flag {c} would always be true"),
+            Error::LongTautology(s) => write!(f, "flag {s} would always be true"),
+            Error::FlexTautology(c, s) => write!(f, "flag {c}|{s} would always be true"),
         }
     }
 }
@@ -42,10 +31,10 @@ impl Parser {
     /// # Errors
     ///
     /// Will return an [`Error`] if the specified variable is already `true`.
-    fn flag(&mut self, variable: &mut bool, name: char, _description: &str) -> Result<()> {
+    fn flag(&mut self, variable: &mut bool, c: char, _description: &str) -> Result<()> {
         (!*variable)
             .then_some(())
-            .ok_or_else(|| Error::Tautology(FlagName::Short(name)))?;
+            .ok_or_else(|| Error::CharTautology(c))?;
 
         // TODO: save arg for use in parsing
 
@@ -87,7 +76,7 @@ mod tests {
         );
         assert_eq!(
             parser.flag(&mut t, 't', "fake flag initialized true"),
-            Err(Error::Tautology(FlagName::Short('t')))
+            Err(Error::CharTautology('t')),
         );
     }
 
