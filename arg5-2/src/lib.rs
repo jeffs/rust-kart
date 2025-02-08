4c48efaea7a31bd2fbdae949ac09980d3ccad9d9
Jeff Schwab <jeff@unbuggy.com>
2025-02-07 19:32:27 -0500

    Implement long flags

--- arg5-2/src/lib.rs
+++ arg5-2/src/lib.rs
@@ -7,6 +7,7 @@ const ASCII_COUNT: usize = 1 << 7;
 
 /// Maps ASCII code points to variables.
 type CharMap<'a, T> = [Option<&'a mut T>; ASCII_COUNT];
+type LongMap<'a, T> = Vec<(&'static str, &'a mut T)>;
 
 #[derive(Debug, PartialEq)]
 pub enum InitError {
@@ -19,8 +20,9 @@ pub enum InitError {
     /// The supplied flag or option name is not supported.  Future versions of this library may be
     /// extended to support non-ASCII and/or non-alphanumeric flag/option names, but the current
     /// version remains conservative in the name of portability.  Note that the current limitations
-    /// apply only to argument names, not values; e.g., args like `--date mañana` are fine.
+    /// apply only to argument names, not values; e.g., args like `--date Mañana` are fine.
     CharName(char),
+    LongName(&'static str),
     /// The supplied flag or option name was bound to multiple target variables.
     CharDup(char),
 }
@@ -32,6 +34,7 @@ impl fmt::Display for InitError {
             InitError::LongTautology(s) => write!(f, "flag --{s} would always be true"),
             InitError::FlexTautology(c, s) => write!(f, "flag -{c}|--{s} would always be true"),
             InitError::CharName(c) => write!(f, "non-ASCII flag name -{c} is unsupported"),
+            InitError::LongName(s) => write!(f, "non-ASCII flag name --{s} is unsupported"),
             InitError::CharDup(c) => write!(f, "flag -{c} cannot be bound to multiple variables"),
         }
     }
@@ -62,6 +65,11 @@ impl FlagName for (char, &'static str) {
     }
 }
 
+/// Returns true if the specified character is supported in flag or option names.
+fn is_long_name(c: char) -> bool {
+    c.is_ascii_alphanumeric() && !c.is_uppercase() || c == '-'
+}
+
 fn parse_char_flag(vars: &mut CharMap<bool>, name: u8) {
     if let Some(Some(var)) = vars.get_mut(usize::from(name)) {
         **var = true;
@@ -76,8 +84,17 @@ fn parse_char_flags(vars: &mut CharMap<bool>, names: &[u8]) {
     }
 }
 
+fn parse_long_flag(vars: &mut LongMap<bool>, name: &str) {
+    for (key, var) in vars.iter_mut() {
+        if *key == name {
+            **var = true;
+        }
+    }
+}
+
 pub struct Parser<'a> {
     char_flags: CharMap<'a, bool>,
+    long_flags: LongMap<'a, bool>,
 }
 
 impl<'a> Parser<'a> {
@@ -88,7 +105,7 @@ impl<'a> Parser<'a> {
         &mut self,
         var: &'a mut bool,
         name: char,
-        _description: &str,
+        _description: &'static str,
     ) -> Result<(), InitError> {
         let byte: u8 = name
             .is_ascii_alphanumeric()
@@ -104,11 +121,33 @@ impl<'a> Parser<'a> {
 
     /// # Errors
     ///
+    /// Will return an [`Error`] if the specified variable is already `true`.
+    pub fn long_flag(
+        &mut self,
+        var: &'a mut bool,
+        name: &'static str,
+        _description: &'static str,
+    ) -> Result<(), InitError> {
+        name.chars()
+            .all(is_long_name)
+            .then_some(())
+            .ok_or(InitError::LongName(name))?;
+        (!*var)
+            .then_some(())
+            .ok_or(InitError::LongTautology(name))?;
+        self.long_flags.push((name, var));
+        Ok(())
+    }
+
+    /// # Errors
+    ///
     /// Will return an [`Error`] if the specified arguments cannot be parsed.
     pub fn parse(mut self, args: impl IntoIterator<Item = OsString>) -> Result<(), ParseError> {
         for arg in args.into_iter().skip(1) {
-            if let [b'-', b'-', _bytes @ ..] = arg.as_bytes() {
-                todo!("long flag");
+            if let [b'-', b'-', bytes @ ..] = arg.as_bytes() {
+                if let Ok(name) = std::str::from_utf8(bytes) {
+                    parse_long_flag(&mut self.long_flags, name);
+                }
             } else if let [b'-', bytes @ ..] = arg.as_bytes() {
                 parse_char_flags(&mut self.char_flags, bytes);
             } else {
@@ -121,6 +160,7 @@ impl<'a> Parser<'a> {
     pub const fn new() -> Parser<'a> {
         Parser {
             char_flags: [const { None }; ASCII_COUNT],
+            long_flags: Vec::new(),
         }
     }
 }
@@ -139,11 +179,12 @@ mod tests {
 
     #[test]
     fn char_flag_non_ascii() {
-        let mut ñ = false;
+        const NAME: char = 'ñ';
+        let mut var = false;
         let mut parser = Parser::new();
         assert_eq!(
-            parser.char_flag(&mut ñ, 'ñ', "fake flag with non-ASCII name ñ"),
-            Err(InitError::CharName('ñ'))
+            parser.char_flag(&mut var, NAME, "fake char flag with non-ASCII name"),
+            Err(InitError::CharName(NAME))
         );
     }
 
@@ -167,6 +208,42 @@ mod tests {
             let mut got = false;
             let mut parser = Parser::new();
             parser.char_flag(&mut got, 'v', "fake flag").unwrap();
+            assert_eq!(parser.parse(args), Ok(()));
+            assert_eq!(got, want);
+        }
+    }
+
+    #[test]
+    fn long_flag_non_ascii() {
+        const NAME: &str = "año";
+        let mut var = false;
+        let mut parser = Parser::new();
+        assert_eq!(
+            parser.long_flag(&mut var, NAME, "fake long flag with non-ASCII name"),
+            Err(InitError::LongName(NAME))
+        );
+    }
+
+    #[test]
+    fn long_flag_tautology() {
+        let (mut f, mut t) = (false, true);
+        let mut parser = Parser::new();
+        assert_eq!(
+            parser.long_flag(&mut f, "this-is-fine", "fake flag initialized false"),
+            Ok(())
+        );
+        assert_eq!(
+            parser.long_flag(&mut t, "this-is-bad", "fake flag initialized true"),
+            Err(InitError::LongTautology("this-is-bad")),
+        );
+    }
+
+    #[test]
+    fn long_flag_parse() {
+        for (args, want) in [(fake_args([]), false), (fake_args(["--fake"]), true)] {
+            let mut got = false;
+            let mut parser = Parser::new();
+            parser.long_flag(&mut got, "fake", "fake flag").unwrap();
             assert_eq!(parser.parse(args), Ok(()));
             assert_eq!(got, want);
         }
