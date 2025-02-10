844ddbd90ffc486193a27b33076ab6455e6dc437
Jeff Schwab <jeff@unbuggy.com>
2025-02-10 07:37:49 -0500

    Add sanity check

--- leap/src/main.rs
+++ leap/src/main.rs
@@ -7,10 +7,10 @@ fn is_friday_the_13th(day: Day, date: Date) -> bool {
 
 fn main() {
     // The first day of 2023 happened to be a Sunday, which is the first item of `days`.
-    let mut date = Date::from_ymd(2023, 1, 1).expect("hard-coded start date should be valid");
+    let mut date = Date::from_ymd(2023, 1, 1).expect("hard-coded start date");
     let mut days = DAYS.iter().cycle();
 
-    let stop = Date::from_ymd(2063, 1, 1).expect("hard-coded stop date should be valid");
+    let stop = Date::from_ymd(2063, 1, 1).expect("hard-coded stop date");
 
     while date != stop {
         if (date.month(), date.day()) == (1, 1) {
@@ -18,8 +18,15 @@ fn main() {
         }
 
         let day = *days.next().expect("there's always tomorrow");
+
+        // Sanity check.
+        #[cfg(debug_assertions)]
+        if date == Date::from_ymd(2025, 2, 10).expect("hard-coded date") {
+            assert_eq!(day, Day::Mon);
+        }
+
         let b = is_friday_the_13th(day, date);
-        print!("{}", if b { 'F' } else { '_' });
+        print!("{}", if b { 'X' } else { ' ' });
 
         if (date.month(), date.day()) == (12, 31) {
             println!("\n")
