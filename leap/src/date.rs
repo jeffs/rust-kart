ed81f77dcd3d2ec13465acb136b825a20a4966a2
Jeff Schwab <jeff@unbuggy.com>
2025-02-10 06:13:07 -0500

    Nominally complete Date type

--- leap/src/date.rs
+++ leap/src/date.rs
@@ -1,7 +1,11 @@
-use std::fmt;
-use std::ops::RangeInclusive;
+use std::ops::{RangeFrom, RangeInclusive};
+use std::{fmt, u16};
 
 use crate::{Error, Result};
+
+const DAYS_PER_WEEK: u8 = 7;
+const MONTHS: RangeInclusive<u8> = 1..=12;
+const YEARS: RangeFrom<u16> = 1..;
 
 fn month_days(year: u16, month: u8) -> RangeInclusive<u8> {
     let month = usize::from(month);
@@ -30,6 +34,38 @@ pub struct Date {
 }
 
 impl Date {
+    /// December 31st, tens of thousands of years from the day you're reading this.
+    pub const MAX: Date = Date {
+        year: u16::MAX,
+        month: 12,
+        day: 31,
+    };
+
+    /// Returns the date [one week later](https://www.youtube.com/watch?v=BKP3Qe_zZ18).
+    ///
+    /// # Panics
+    ///
+    /// Will panic in debug mode only if the resulting date would exceed [`Date::MAX`].
+    pub fn plus_one_week(self) -> Date {
+        let month_days = month_days(self.year, self.month);
+        let day = self.day + DAYS_PER_WEEK;
+        if month_days.contains(&day) {
+            return Date { day, ..self };
+        }
+
+        let day = day - month_days.end();
+        let month = self.month + 1;
+        if MONTHS.contains(&month) {
+            return Date { month, day, ..self };
+        }
+
+        Date {
+            year: self.year + 1,
+            month: 1,
+            day,
+        }
+    }
+
     pub fn day(self) -> u8 {
         self.day
     }
@@ -50,7 +86,7 @@ impl Date {
     /// ```
     #[must_use]
     pub fn from_ymd(year: u16, month: u8, day: u8) -> Result<Date> {
-        (0 < year && (1..=12).contains(&month) && month_days(year, month).contains(&day))
+        (YEARS.contains(&year) && MONTHS.contains(&month) && month_days(year, month).contains(&day))
             .then_some(Date { year, month, day })
             .ok_or(Error::Date { year, month, day })
     }
