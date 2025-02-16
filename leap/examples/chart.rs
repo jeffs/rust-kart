be54bd69e44a1b810eb72390d9a9fe818425d6c7
Jeff Schwab <jeff@unbuggy.com>
2025-02-16 09:51:10 -0500

    Further example cleanup; remove debug assertion

--- leap/examples/chart.rs
+++ leap/examples/chart.rs
@@ -1,28 +1,22 @@
+use std::iter;
+
 use leap::week::Day;
 use leap::Date;
 
 fn main() {
     for year in 1800..2200 {
-        print!("{year}: ");
-
-        let mut date = Date::from_ymd(year - 1, 12, 31).expect("last day before starting year");
-        while {
-            date = date.plus_one_day();
-            date.year() == year
-        } {
-            if date.day_of_week() == Day::Fri && date.day() == 13 {
-                print!("X");
+        let marks = iter::successors(Date::from_ymd(year, 1, 1).ok(), |today| {
+            let tomorrow = today.plus_one_day();
+            (tomorrow.year() == year).then_some(tomorrow)
+        })
+        .map(|date| {
+            if (date.day_of_week(), date.day()) == (Day::Fri, 13) {
+                'X'
             } else {
-                print!(" ");
+                ' '
             }
-
-            // Sanity check.
-            #[cfg(debug_assertions)]
-            if date == Date::from_ymd(2025, 2, 10).expect("hard-coded date") {
-                assert_eq!(date.day_of_week(), Day::Mon);
-            }
-        }
-
-        println!();
+        })
+        .collect::<String>();
+        println!("{year}: {marks}");
     }
 }
