a87b0a88a154a8b5c49b1949050845685a91f089
Jeff Schwab <jeff@unbuggy.com>
2025-04-13 09:41:07 -0400

    Support Zellij in tmux-send

--- tmux-send/src/main.rs
+++ tmux-send/src/main.rs
@@ -1,4 +1,6 @@
-//! Sends standard input to the bottom-right tmux pane.
+//! Sends standard input to the bottom-right tmux, or next zellij, pane.
+
+use std::env;
 use std::io::{self, Read};
 use std::os::unix::process::ExitStatusExt;
 use std::process::{self, Command, ExitStatus};
@@ -8,10 +10,28 @@ fn read_stdin() -> io::Result<Vec<u8>> {
     io::stdin().read_to_end(&mut bytes).map(|_| bytes)
 }
 
+fn run_tmux(text: &str) -> io::Result<ExitStatus> {
+    let args = ["send-keys", "-lt", "bottom-right", "--", text];
+    Command::new("tmux").args(args).status()
+}
+
+fn zellij_action<'a>(args: impl IntoIterator<Item = &'a str>) -> io::Result<ExitStatus> {
+    Command::new("zellij").arg("action").args(args).status()
+}
+
+fn run_zellij(text: &str) -> io::Result<ExitStatus> {
+    zellij_action(["focus-next-pane"])?;
+    zellij_action(["write-chars", text])?;
+    zellij_action(["focus-previous-pane"])
+}
+
 fn run() -> Result<ExitStatus, Box<dyn std::error::Error>> {
     let text = String::from_utf8(read_stdin()?)?;
-    let args = ["send-keys", "-lt", "bottom-right", "--", &text];
-    Ok(Command::new("tmux").args(args).status()?)
+    Ok(if env::var_os("ZELLIJ").is_some() {
+        run_zellij(&text)?
+    } else {
+        run_tmux(&text)?
+    })
 }
 
 fn main() {
