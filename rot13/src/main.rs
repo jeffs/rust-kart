791cc22196414296e221ccfa9f05819d694e8479
Jeff Schwab <jeff@unbuggy.com>
2025-05-06 14:00:37 -0400

    Add rot13

---
+++ rot13/src/main.rs
@@ -0,0 +1,19 @@
+use std::io::{self, Read, Write};
+
+fn main() {
+    // Stdin and stdout are both typically line-buffered.
+    for byte in io::stdin().bytes() {
+        let Ok(byte) = byte else {
+            return;
+        };
+        let byte = match byte {
+            b'a'..=b'z' => b'a' + (byte - b'a' + 13) % 26,
+            b'A'..=b'Z' => b'A' + (byte - b'A' + 13) % 26,
+            b'0'..=b'9' => b'0' + (byte - b'0' + 5) % 10,
+            _ => byte,
+        };
+        if io::stdout().write_all(&[byte]).is_err() {
+            return;
+        }
+    }
+}
