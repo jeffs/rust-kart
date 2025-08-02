8948b48d82140ad3f75adcb7f9f57ff54ae377e1
Jeff Schwab <jeff@unbuggy.com>
2025-08-02 07:49:16 -0400

    Overthink arg parsing

--- grit/src/main.rs
+++ grit/src/main.rs
@@ -122,9 +122,7 @@ async fn update(args: env::ArgsOs) -> Result<()> {
 
     let orig = git(["rev-parse", "--abbrev-ref", "HEAD"]).await?;
     let orig = orig.as_str().trim();
-
     let trunk = trunk::local().await?;
-
     if trunk != orig {
         git(["checkout", &trunk]).await?;
     }
@@ -178,38 +176,46 @@ async fn update(args: env::ArgsOs) -> Result<()> {
     Ok(())
 }
 
-/// Returns Git args and, if specified, base branch name.
-fn since_args(our_args: env::ArgsOs) -> Result<(Vec<ffi::OsString>, Option<ffi::OsString>)> {
-    let mut base: Option<ffi::OsString> = None;
-    let mut git_args = [
-        "log",
-        "--color=always",
-        "--decorate",
-        "--first-parent",
-        "--graph",
-        "--oneline",
-    ]
-    .map(ffi::OsString::from)
-    .to_vec();
-    for os in our_args {
-        let Some(s) = os.to_str() else {
-            return Err(Error::Arg(os));
-        };
-        if s.starts_with('-') {
-            git_args.push(os);
-        } else if base.is_none() {
-            base = Some(os);
-        } else {
-            return Err(Error::Arg(os));
+struct SinceArgs {
+    /// Arguments to be forwarded to git.
+    git_args: Vec<ffi::OsString>,
+    /// The base branch name, if specified.
+    base: Option<ffi::OsString>,
+}
+
+impl SinceArgs {
+    fn new(our_args: env::ArgsOs) -> Result<SinceArgs> {
+        let mut base: Option<ffi::OsString> = None;
+        let mut git_args = [
+            "log",
+            "--color=always",
+            "--decorate",
+            "--first-parent",
+            "--graph",
+            "--oneline",
+        ]
+        .map(ffi::OsString::from)
+        .to_vec();
+        for os in our_args {
+            let Some(s) = os.to_str() else {
+                return Err(Error::Arg(os));
+            };
+            if s.starts_with('-') {
+                git_args.push(os);
+            } else if base.is_none() {
+                base = Some(os);
+            } else {
+                return Err(Error::Arg(os));
+            }
         }
+        Ok(SinceArgs { git_args, base })
     }
-    Ok((git_args, base))
 }
 
 /// Lists commmits reachable from HEAD, but not from a specified base branch
 /// (which defaults to the local trunk).
 async fn since(our_args: env::ArgsOs) -> Result<()> {
-    let (mut git_args, base) = since_args(our_args)?;
+    let SinceArgs { mut git_args, base } = SinceArgs::new(our_args)?;
     let range = match base {
         Some(some) => format!("{}..", some.display()),
         None => format!("{}..", trunk::local().await?),
@@ -222,7 +228,7 @@ async fn since(our_args: env::ArgsOs) -> Result<()> {
 /// Summarizes changes to the working copy (relative to HEAD), then lists
 /// commmits from trunk (or a specified base) to HEAD, inclusive.
 async fn since_long(our_args: env::ArgsOs) -> Result<()> {
-    let (mut git_args, base) = since_args(our_args)?;
+    let SinceArgs { mut git_args, base } = SinceArgs::new(our_args)?;
     print!("{}", git(["diff", "--stat"]).await?);
     let range = match base {
         Some(some) => format!("{}^...", some.display()),
