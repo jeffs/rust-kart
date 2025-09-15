eabe01dc4e26cd9fe7094a440e506ea3bb194741
Jeff Schwab <jeff@unbuggy.com>
2025-09-15 10:59:51 -0400

    Rename rk-lev to lev
    I'll eventually implement some work of namespace, such as an `rk` supercommand, or automatically symlinking from an `rk/bin` directory.

--- lev/src/main.rs
+++ lev/src/main.rs
@@ -1,4 +1,4 @@
-use rk_lev::lev;
+use lev::lev;
 
 fn print_lev(a: &str, b: &str) {
     println!("{:4} {b}", lev(a.chars(), b.chars()));
@@ -8,7 +8,7 @@ fn main() {
     let mut args = std::env::args();
     let (Some(a), Some(b)) = (args.nth(1), args.next()) else {
         eprintln!("Usage: lev STRING1 STRING2 [STRINGS...]");
-        std::process::exit(2);
+        std::process::exit(2)
     };
     print_lev(&a, &b);
     for b in args {
