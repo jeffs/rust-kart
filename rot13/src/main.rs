5306abe40a5beca9d5019b247f85a37b541dade7
Jeff Schwab <jeff@unbuggy.com>
2025-10-28 12:34:57 -0400

    Accept files in rot13

--- rot13/src/main.rs
+++ rot13/src/main.rs
@@ -1,18 +1,45 @@
-use std::io::{self, Read, Write};
+use std::{
+    fs,
+    io::{self, Read, Write},
+    path::Path,
+};
+
+fn rot13(byte: u8) -> u8 {
+    match byte {
+        b'a'..=b'z' => b'a' + (byte - b'a' + 13) % 26,
+        b'A'..=b'Z' => b'A' + (byte - b'A' + 13) % 26,
+        b'0'..=b'9' => b'0' + (byte - b'0' + 5) % 10,
+        _ => byte,
+    }
+}
+
+/// # Panics
+///
+/// Will panic if stdout cannot be written.
+fn write_rot13_bytes(bytes: impl IntoIterator<Item = u8>) {
+    for byte in bytes {
+        io::stdout().write_all(&[rot13(byte)]).unwrap();
+    }
+}
+/// # Panics
+///
+/// Will panic if stdin cannot be read.
+fn process_stdin() {
+    write_rot13_bytes(io::BufReader::new(io::stdin()).bytes().map(Result::unwrap));
+}
+
+fn process_file(path: &Path) {
+    let bytes = fs::read(path).unwrap();
+    write_rot13_bytes(bytes);
+}
 
 fn main() {
-    for byte in io::BufReader::new(io::stdin()).bytes() {
-        let Ok(byte) = byte else {
-            return;
-        };
-        let byte = match byte {
-            b'a'..=b'z' => b'a' + (byte - b'a' + 13) % 26,
-            b'A'..=b'Z' => b'A' + (byte - b'A' + 13) % 26,
-            b'0'..=b'9' => b'0' + (byte - b'0' + 5) % 10,
-            _ => byte,
-        };
-        if io::stdout().write_all(&[byte]).is_err() {
-            return;
+    let args = std::env::args_os().skip(1).collect::<Vec<_>>();
+    if args.is_empty() {
+        process_stdin();
+    } else {
+        for path in args {
+            process_file(Path::new(&path));
         }
     }
 }
